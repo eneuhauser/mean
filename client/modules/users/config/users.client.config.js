d2671336c711d257ef3e959f5edb1d090858b772
Eric Neuhauser <eneuhauser@gmail.com>
2014-12-05 15:37:22 -0600

    Removing more annotations

--- client/modules/users/config/users.client.config.js
+++ client/modules/users/config/users.client.config.js
@@ -3,26 +3,24 @@
 // Config HTTP Error Handling
 angular.module('users').config(function($httpProvider) {
 	// Set the httpProvider "not authorized" interceptor
-	$httpProvider.interceptors.push(['$q', '$location', 'Authentication',
-		function($q, $location, Authentication) {
-			return {
-				responseError: function(rejection) {
-					switch (rejection.status) {
-						case 401:
-							// Deauthenticate the global user
-							Authentication.user = null;
+	$httpProvider.interceptors.push(function($q, $location, Authentication) {
+		return {
+			responseError: function(rejection) {
+				switch (rejection.status) {
+					case 401:
+						// Deauthenticate the global user
+						Authentication.user = null;
 
-							// Redirect to signin page
-							$location.path('signin');
-							break;
-						case 403:
-							// Add unauthorized behaviour
-							break;
-					}
-
-					return $q.reject(rejection);
+						// Redirect to signin page
+						$location.path('signin');
+						break;
+					case 403:
+						// Add unauthorized behaviour
+						break;
 				}
-			};
-		}
-	]);
+
+				return $q.reject(rejection);
+			}
+		};
+	});
 });
